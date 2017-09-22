9190fb40966678c3de12dc6020f16ce38ae99bc1
Jonty Lee <ljlbj@foxmail.com>
2017-09-22 14:20:08 +0800

    navbar design

--- yaf/public/js/main.js
+++ yaf/public/js/main.js
@@ -7,11 +7,22 @@ requirejs.config({
     },
     shim: {
         semanticUI: {
-            deps: ['jquery', 'css!../semantic/dist/semantic.min']
+            deps: ['jquery']
+        }
+    },
+    map: {
+        '*':{
+            'css': 'css.min'
         }
     }
 });
-requirejs(['jquery'], ($) => {
-    var title = $('title');
-    console.log(title.html());
+requirejs(['jquery', 'semanticUI'], ($, semanticUI) => {
+    $('.ui.navbar .search-icon > i').click(function () {
+        let icon = $(this);
+        if (icon.text() === 'search') {
+            icon.text('close').parents('.content').addClass('searching');
+        } else {
+            icon.text('search').parents('.content').removeClass('searching');
+        }
+    })
 });
